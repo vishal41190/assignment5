f19d12a31125211566461797aa24d35d0ec05413
vishal <vishal41190@gmail.com>
2015-03-16 19:33:07 -0700

    Added 1 ref. in server.js for math random function

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var http = require("http"),
     querystring = require("querystring"),
     child_process = require("child_process");
 
-
+//http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
